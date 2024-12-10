60a1dc7ea1e807d145173843a9e00498396a72e2
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-12-10 15:01:12 -0700

    feat: implement basic HTML rendering and server setup for Todo application

---
+++ src/core/html.ts
@@ -0,0 +1,6 @@
+export const html = (strings: TemplateStringsArray, ...values: any[]) => {
+  return strings.reduce((acc, str, i) => {
+    const value = values[i] || "";
+    return acc + str + value;
+  }, "");
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,40 @@
+import { html } from "./core/html";
+
+const viewDoc = (body: string) => html`
+  <html>
+    <head>
+      <title>Todo</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      ${body}
+    </body>
+  </html>
+`;
+
+const viewIndex = () => html`
+  <main>
+    <section>
+      <h1>Todo</h1>
+    </section>
+  </main>
+`;
+
 export const main = () => {
-  console.log("Hello World");
+  const server = Bun.serve({
+    fetch(request, _server) {
+      return new Response(viewDoc(viewIndex()), {
+        headers: {
+          "content-type": "text/html",
+        },
+      });
+    },
+  });
+
+  console.log(`Server started at http://localhost:${server.port}`);
 };

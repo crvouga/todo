bd057eb453e4161ef49031ffc48b267c889d2e5b
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2025-01-08 14:02:26 -0700

    feat: add configurable delay for set and get operations in hash-map implementation

--- src/core/key-value-db/impl-hash-map.ts
+++ src/core/key-value-db/impl-hash-map.ts
@@ -5,17 +5,18 @@ import { IKeyValueDb } from "./interface.ts";
 export type Config = {
   t: "hash-map";
   hashMap: Map<string, string>;
+  delay?: number;
 };
 
 export const KeyValueDb = (config: Config): IKeyValueDb => {
   return {
     async set(key, value) {
-      await delay(100);
+      await delay(config.delay ?? 0);
       config.hashMap.set(key, value);
       return Ok(null);
     },
     async get(key) {
-      await delay(100);
+      await delay(config.delay ?? 0);
       return Ok(config.hashMap.get(key) ?? null);
     },
   };
